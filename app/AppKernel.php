d72d08d85937f1ca803a80fd5407f75a5c9aeb95
Netvlies CI <mdekrijger@netvlies.nl>
2012-01-19 11:35:01 +0100

    Fixed deps and updated config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Netvlies\PublishBundle\NetvliesPublishBundle(),
-            new OpenSky\Bundle\LdapBundle\OpenSkyLdapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
